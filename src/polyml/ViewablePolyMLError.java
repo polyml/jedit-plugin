ab37b673c1e38b79bc945dc5930d52a5bfd3add4
Graham <no _author@no_author>
2010-06-09 15:15:09 +0000

    adding a polyml error whose position in a buffer can be determined dynamically
    git-svn-id: svn://svn.code.sf.net/p/polyml/code/trunk/jedit-plugin@1172 ae7d391e-3f74-4a2b-a0db-a8776840fd2a

---
+++ src/polyml/ViewablePolyMLError.java
@@ -0,0 +1,61 @@
+package polyml;
+
+import javax.swing.text.Position;
+import org.gjt.sp.jedit.Buffer;
+
+/**
+ * A PolyML error with flexible error positions.
+ * All PolyMLErrors constructed using this class gain flexible offset positions,
+ * once associated with a buffer.
+ */
+public class ViewablePolyMLError extends PolyMLError {
+
+	protected Buffer buffer;
+	protected Position startOffsetPos;
+	protected Position endOffsetPos;
+	
+	ViewablePolyMLError(char kind, int startPos, int endPos, String msg){
+		super(kind, startPos, endPos, msg);
+		startOffsetPos = null;
+		endOffsetPos = null;
+	}
+	
+	/**
+	 * Associates this error with a buffer, assigning flexible buffer-relative
+	 * positions to the offsets specified in the error. 
+	 */
+	public void associateBuffer(Buffer b) {
+		if (b != null) {
+			buffer = b;
+			startOffsetPos = b.createPosition(startPos);
+			endOffsetPos = b.createPosition(endPos);
+		} else {
+			startOffsetPos = null;
+			endOffsetPos = null;
+		}
+	}
+	
+	/** Returns the start position of the error */
+	public int getStartPos() {
+		if (startOffsetPos != null) {
+			return startOffsetPos.getOffset();
+		}
+		return super.getStartPos();
+	}
+	
+	/** Returns the end position of the error. */
+	public int getEndPos() {
+		if (endOffsetPos != null) {
+			return endOffsetPos.getOffset();
+		}
+		return super.getEndPos();
+	}
+	
+	/** Returns the line number */
+	public Integer getLineNumber() {
+		if (buffer != null) {
+			return buffer.getLineOfOffset(startOffsetPos.getOffset());
+		}
+		return null;
+	}
+}
