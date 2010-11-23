b8912eb44c8e298ee61716eae62aaa8647364c49
Graham <no _author@no_author>
2010-11-23 13:26:18 +0000

    unused imports
    git-svn-id: svn://svn.code.sf.net/p/polyml/code/trunk/jedit-plugin@1236 ae7d391e-3f74-4a2b-a0db-a8776840fd2a

--- src/polyml/StateViewDocument.java
+++ src/polyml/StateViewDocument.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Enumeration;
 
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Element;
